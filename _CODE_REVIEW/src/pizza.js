fd14b65de4dbc7ba95d03ded8a294b1b5f920649
cort17 <156112210+cort17@users.noreply.github.com>
2024-03-21 23:49:04 +0300

    Add files via upload

---
+++ _CODE_REVIEW/src/pizza.js
@@ -0,0 +1,58 @@
+export const pizza = [
+    {
+        title: "Пицца Суприм",
+        desc: "Куриная грудка маринованная, болгарский перец, баклажаны, соевый соус, шампиньоны, специальный соус, помидоры, моцарелла, чесночное масло",
+        price: "100",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza/Suprim-600x600.png",
+        diameter: "33"
+    },
+    {
+        title: "Пицца Кантри",
+        desc: "Бекон, маринованная свинина, моцарелла, шампиньоны, фирменный соус, маринованные огурцы, лук",
+        price: "120",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza/Kantri-600x600.png",
+        diameter: "33"
+    },
+    {
+        title: "Пицца с лососем и песто",
+        desc: "Копченый лосось, моцарелла, специальный соус, руккола, помидоры, песто",
+        price: "150",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza/Losos-i-pesto-600x600.png",
+        diameter: "33"
+    },
+    {
+        title: "Пицца Карбонара",
+        desc: "Нежный сливочный соус карбонара, фермерская моцарелла, ломтики бекона, ветчина, помидоры",
+        price: "200",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza_vitrina/picca-karbonara-600x600.jpg",
+        diameter: "33"
+    },
+    {
+        title: "Пицца Грибная",
+        desc: "В его основе фирменный нежный соус из свежих шампиньонов, сливок и молока с добавлением сливочного масла, фирменных специй под шапочкой фермерской моцареллы.",
+        price: "150",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza_vitrina/picca-gribnoy-julen-600x600.jpg",
+        diameter: "33"
+    },
+    {
+        title: "Папа Пицца",
+        desc: "Моцарелла, томатный соус, маринованная курица, колбаса пепперони, бекон, шампиньоны, перец болгарский, луковые кольца",
+        price: "180",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza_vitrina/picca-papa-600x600.jpg",
+        diameter: "33"
+    },
+    {
+        title: "Пицца Морская",
+        desc: "Королевские креветки, лосось, кольца кальмара, помидоры черри, моцарелла, свежие",
+        price: "200",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza_vitrina/picca-morskaya-600x600.jpg",
+        diameter: "33"
+    },
+    {
+        title: "Пицца Цезарь",
+        desc: "Свежий салат айсберг, запеченная куриная грудка, помидоры черри, моцарелла и сыр пармезан, соус Цезарь",
+        price: "250",
+        photo: "https://japanmoose.ru/image/cache/catalog/products/pizza/Pizza_vitrina/picca-cezar-600x600.jpg",
+        diameter: "33"
+    }
+]
